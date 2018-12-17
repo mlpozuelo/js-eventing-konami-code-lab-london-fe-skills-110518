21f538b4e0e13eb8e417d434a55e48bb0d6c135d
Maria Lopez Pozuelo <mlpozuelo@google.com>
2018-12-17 19:13:53 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,16 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  const code = document.getElementByID('code')
   
-  get
-  code.addEventListener('keydown', )
+  code.addEventListener('keydown', function (event) { alert ('Congratulations!)') )
   
 
 }
-
-
-const main = document.getElementById('main')
- 
-main.addEventListener('click', function(event) {
-  alert('I was clicked!')
-})
