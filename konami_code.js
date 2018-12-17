3c6481329ebe21da010c0e76ca6daca53859b8f8
Maria Lopez Pozuelo <mlpozuelo@google.com>
2018-12-17 19:34:17 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(keydown) {
+function init() {
   // Write your JavaScript code inside the init() function
   
-  
-  if(keydown == 'code')
-  
-  code.addEventListener('keydown', function (event) { alert ('Congratulations!')
-  
-  }
+ 
