b99a372cab4df518b88c519c21c8444dc29e21f3
Maria Lopez Pozuelo <mlpozuelo@google.com>
2018-12-17 19:18:38 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+function init(keydown) {
   // Write your JavaScript code inside the init() function
+  if()
+  
   const code = document.getElementByID('code')
   
   code.addEventListener('keydown', function (event) { alert ('Congratulations!')
   
 
 }
+}
+
+
+function eventHandler(event) {
+  if (event.type == 'fullscreenchange') {
+    /* handle a full screen toggle */
+  } else /* fullscreenerror */ {
+    /* handle a full screen toggle error */
+  }
 }
